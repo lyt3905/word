5276993b1d3172977d044e0e0c22476357664535
lyt <1003655329@qq.com>
2019-11-07 23:23:42 +0800

    一个用来添加单词的提示框

--- app/src/main/java/cn/edu/bistu/cs/se/myapplication/MainActivity.java
+++ app/src/main/java/cn/edu/bistu/cs/se/myapplication/MainActivity.java
@@ -32,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
     private MyDataBaseHelper dbHelper;
     private SQLiteDatabase sqlDB ;
     EditText et;
-    ArrayList<Map<String, String>> result;
+    ArrayList<Map<String, Object>> result;
     ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,12 +45,65 @@ public class MainActivity extends AppCompatActivity {
                 "myDict.db3", 1);;
         sqlDB = dbHelper.getReadableDatabase();;
         listView = (ListView) findViewById(R.id.listView1);
-       firstListview();
-       listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        firstListview();
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
            @Override
-           public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+           public void onItemClick(AdapterView<?> adapterView, View view, final int i, long l) {
                Map map=result.get(i);
-               System.out.println(map.get("word"));;
+               final int id=(int)map.get("id");
+               String word=map.get("word")+"";
+               String mean_word=map.get("mean_word")+"";
+               String example_sentence=map.get("example_sentence")+"";
+               System.out.println("id="+id);
+               System.out.println("word="+word);
+               System.out.println("mean_word="+mean_word);
+               System.out.println("example_sentence="+example_sentence);
+
+               android.app.AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+               final LayoutInflater inflater = MainActivity.this.getLayoutInflater();
+               final View view_custom = inflater.inflate(R.layout.add, null,false);
+               final EditText wordet =view_custom.findViewById(R.id.word);
+               final EditText mean_wordet=view_custom.findViewById(R.id.word_mean);
+               final EditText example_sentenceet =view_custom.findViewById(R.id.example_sentence);
+
+               wordet.setText(word);
+               mean_wordet.setText(mean_word);
+               example_sentenceet.setText(example_sentence);
+               builder.setView(view_custom);
+               builder.setCancelable(true);
+               builder.setTitle("修改单词");
+               builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                   @Override
+                   public void onClick(DialogInterface dialog, int which) {
+
+
+
+                       String strword = wordet.getText().toString();
+                       String strword_mean = mean_wordet.getText().toString();
+                       String strexample_sentence=example_sentenceet.getText().toString();
+
+                       sqlDB.execSQL("update dict set word =? , mean_word =? , example_sentence =? where _id = ? ",
+                               new Object[] {strword, strword_mean, strexample_sentence,id});
+                       System.out.println("after-id="+id);
+                       System.out.println("after-strword="+strword);
+                       System.out.println("after-strword_mean="+strword_mean);
+                       System.out.println("after-strexample_sentence="+strexample_sentence);
+                       Toast.makeText(getApplicationContext(), "修改成功", Toast.LENGTH_LONG)
+                               .show();
+                       firstListview();
+
+
+                   }
+               });
+               builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                   @Override
+                   public void onClick(DialogInterface dialog, int which) {
+
+                   }
+               });
+               builder.show();
+               AlertDialog alert=builder.create();
+
            }
        });
     }
@@ -88,13 +141,13 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public void onClick(DialogInterface dialog, int which) {
 
-                    Toast.makeText(getApplicationContext(), "ainiyou", Toast.LENGTH_LONG)
-                            .show();
+
 
 
                     EditText word =view_custom.findViewById(R.id.word);
                     EditText word_mean=view_custom.findViewById(R.id.word_mean);
                     EditText example_sentence =view_custom.findViewById(R.id.example_sentence);
+
 
                     String strword = word.getText().toString();
                     String strword_mean = word_mean.getText().toString();
@@ -138,16 +191,16 @@ public class MainActivity extends AppCompatActivity {
                 System.out.println("et=" + key);
 
                 Cursor cursor = dbHelper.getReadableDatabase().rawQuery(
-                        "SELECT * FROM dict WHERE word LIKE ? OR mean_word LIKE ? OR example_sentence LIKE ?",
-                        new String[]{"%" + key + "%", "%" + key + "%", "%" + key + "%"});
-                ArrayList<Map<String, String>> result = new ArrayList<Map<String, String>>();
+                            "SELECT * FROM dict WHERE word LIKE ? OR mean_word LIKE ? OR example_sentence LIKE ?",
+                            new String[]{"%" + key + "%", "%" + key + "%", "%" + key + "%"});
+                    ArrayList<Map<String, String>> result = new ArrayList<Map<String, String>>();
 
-                while (cursor.moveToNext()) {
-                    Map<String, String> map = new HashMap<String, String>();
-                    map.put("word", cursor.getString(1));
-                    map.put("mean_word", cursor.getString(2));
-                    map.put("example_sentence", cursor.getString(3));
-                    result.add(map);
+                    while (cursor.moveToNext()) {
+                        Map<String, String> map = new HashMap<String, String>();
+                        map.put("word", cursor.getString(1));
+                        map.put("mean_word", cursor.getString(2));
+                        map.put("example_sentence", cursor.getString(3));
+                        result.add(map);
                 }
                 SimpleAdapter simpleAdapter = new SimpleAdapter(
                         getApplicationContext(), result, R.layout.line, new String[]{
@@ -169,9 +222,10 @@ public class MainActivity extends AppCompatActivity {
         Cursor cursor = dbHelper.getReadableDatabase().rawQuery(
                 "SELECT * FROM dict WHERE word LIKE ? OR mean_word LIKE ? OR example_sentence LIKE ?",
                 new String[]{"%" + key + "%", "%" + key + "%", "%" + key + "%"});
-        result = new ArrayList<Map<String, String>>();
+        result = new ArrayList<Map<String, Object>>();
         while (cursor.moveToNext()) {
-            Map<String, String> map = new HashMap<String, String>();
+            Map<String, Object> map = new HashMap<String, Object>();
+            map.put("id",cursor.getInt(0));
             map.put("word", cursor.getString(1));
             map.put("mean_word", cursor.getString(2));
             map.put("example_sentence", cursor.getString(3));
--- app/src/main/java/cn/edu/bistu/cs/se/myapplication/MyDataBaseHelper.java
+++ app/src/main/java/cn/edu/bistu/cs/se/myapplication/MyDataBaseHelper.java
@@ -12,7 +12,11 @@ public class MyDataBaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String sql = "CREATE TABLE dict(_id INTEGER PRIMARY KEY autoincrement ,word,mean_word,example_sentence)";
+        String sql = "CREATE TABLE dict(" +
+                "_id INTEGER PRIMARY KEY autoincrement ," +
+                "word," +
+                "mean_word," +
+                "example_sentence)";
         db.execSQL(sql);
     }
 
